5a2fb17fce9f9a33cf4b77b6ecc7a0f0d2a537fb
Terry-zh <496971418@qq.com>
2019-12-13 11:02:40 +0800

    [initial]kt func

---
+++ src/main/kotlin/com/terryzh/BuiltinTypes/functions/JavaMethods.java
@@ -0,0 +1,9 @@
+package com.terryzh.BuiltinTypes.functions;
+
+import java.util.Arrays;
+
+public class JavaMethods {
+    public static void main(String... args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
