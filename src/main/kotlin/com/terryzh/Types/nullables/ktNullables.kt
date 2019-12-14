9c80bbe4b2b6e3f3bc4ff895ef97698e0ca71a25
Terry-zh <496971418@qq.com>
2019-12-14 10:07:28 +0800

    [initial]kt func

---
+++ src/main/kotlin/com/terryzh/Types/nullables/ktNullables.kt
@@ -0,0 +1,47 @@
+package com.terryzh.Types.nullables
+
+import java.io.File
+
+fun main() {
+//    var nonnull: String = "hello"
+//    nonnull = null
+//    val len = nonnull.length
+
+    var nullable: String? = "hello"
+//    val len = nullable?.length
+    val len = nullable?.length ?: 0 // elvis boolean? a: b
+
+    var x: String = "Hello"
+    var y: String? = "World"
+
+//    x = y // Type mismatch
+    y = x // OK
+
+    var a: Int = 2
+    var b: Number = 10.0
+
+//    a = b // Type mismatch
+    b = a // OK
+
+
+
+
+
+
+
+
+
+
+
+
+
+    val person = Person()
+    val title = person.title
+
+    val titleLength = title?.length
+
+    val file = File("abc")
+
+    val files = file.listFiles()
+    println(files.size)
+}
